dea5b06233683b6fdcc9d2648dcf5d699b381a28
Mitchell Skaggs <skaggsm333@gmail.com>
2015-04-22 14:43:01 -0500

    Expanded MathUtil class.

--- 2015-2016_Code/src/com/electronauts/mathutil/MathUtil.java
+++ 2015-2016_Code/src/com/electronauts/mathutil/MathUtil.java
@@ -1,5 +1,8 @@
 package com.electronauts.mathutil;
 
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+
 // TODO: Auto-generated Javadoc
 /**
  * The Class MathUtil.
@@ -19,10 +22,57 @@ public class MathUtil
 		return !(Double.isNaN(d) || Double.isInfinite(d));
 	}
 
+	public static double distance(Point2D p1, Point2D p2)
+	{
+		return distance(p1.getX(), p1.getY(), p2.getX(), p2.getY());
+	}
+
+	public static double distance(double x1, double y1, double x2, double y2)
+	{
+		return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+	}
+
 	/**
-	 * Instantiates a new math util.
+	 * All methods are static.
 	 */
 	private MathUtil()
 	{
+	}
+
+	public static double angleTo(Point2D p1, Point2D p2)
+	{
+		return Math.atan2(p2.getY() - p1.getY(), p2.getX() - p1.getX());
+	}
+
+	public static Point2D getIntersectionPoint(final Point2D p1, final Point2D p2, final Point2D p3, final Point2D p4)
+	{
+		// Lines from p1 to p2 and from p3 to p4
+		// Returns null if they do not intersect
+		final double denominator = (p1.getX() - p2.getX()) * (p3.getY() - p4.getY()) - (p1.getY() - p2.getY()) * (p3.getX() - p4.getX());
+		if (denominator == 0)
+			return null;
+		else
+		{
+			final double top1 = p1.getX() * p2.getY() - p1.getY() * p2.getX();
+			final double top2 = p3.getX() * p4.getY() - p3.getY() * p4.getX();
+			final Point2D.Double p = new Point2D.Double((top1 * (p3.getX() - p4.getX()) - (p1.getX() - p2.getX()) * top2) / denominator, (top1
+					* (p3.getY() - p4.getY()) - (p1.getY() - p2.getY()) * top2)
+					/ denominator);
+			if (isInRange(p.x, p1.getX(), p2.getX()) && isInRange(p.x, p3.getX(), p4.getX()) && isInRange(p.y, p1.getY(), p2.getY())
+					&& isInRange(p.y, p3.getY(), p4.getY()))
+				return p;
+			else
+				return null;
+		}
+	}
+
+	public static Point2D getIntersectionPoint(Line2D l1, Line2D l2)
+	{
+		return getIntersectionPoint(l1.getP1(), l1.getP2(), l2.getP1(), l2.getP2());
+	}
+
+	public static boolean isInRange(final double value, final double range1, final double range2)
+	{
+		return range2 > range1 ? value > range1 && value < range2 : value > range2 && value < range1;
 	}
 }
--- 2015-2016_Code/src/com/electronauts/mathutil/Vector2D.java
+++ 2015-2016_Code/src/com/electronauts/mathutil/Vector2D.java
@@ -1,6 +1,15 @@
 package com.electronauts.mathutil;
 
+import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.Path2D;
 import java.awt.geom.Point2D;
+
+import javax.swing.JComponent;
+import javax.swing.JFrame;
 
 // TODO: Auto-generated Javadoc
 /**
@@ -8,15 +17,35 @@ import java.awt.geom.Point2D;
  */
 public class Vector2D
 {
+	public static void main(String[] args)
+	{
+		final Vector2D v1 = Vector2D.createFromPolar(Math.PI / 3, 100);
+		JComponent comp = new JComponent()
+		{
+			private static final long	serialVersionUID	= 1L;
+
+			@Override
+			public void paint(Graphics g)
+			{
+				Graphics2D g2d = (Graphics2D) g;
+				g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+				v1.paintAt(20, 20, g2d);
+			}
+		};
+		JFrame f = new JFrame();
+		f.add(comp);
+		f.setSize(400, 400);
+		f.setVisible(true);
+	}
 
 	/**
-	 * Adds the.
+	 * Adds the two {@code Vector2D} objects together into a new composite {@code Vector2D}
 	 *
 	 * @param v1
-	 *            the v1
+	 *            the first {@code Vector2D}
 	 * @param v2
-	 *            the v2
-	 * @return the vector2 d
+	 *            the second {@code Vector2D}
+	 * @return the addition of {@code v1} and {@code v2}
 	 */
 	public static Vector2D add(final Vector2D v1, final Vector2D v2)
 	{
@@ -24,13 +53,13 @@ public class Vector2D
 	}
 
 	/**
-	 * Creates the from cartesian.
+	 * Creates a new Vector2D from a Cartesian. The Vector stretches from the origin to the specified point.
 	 *
 	 * @param x
-	 *            the x
+	 *            the x coordinate of the end point of the vector
 	 * @param y
-	 *            the y
-	 * @return the vector2 d
+	 *            the y coordinate of the end point of the vector
+	 * @return a new Vector2D
 	 */
 	public static Vector2D createFromCartesian(final double x, final double y)
 	{
@@ -38,11 +67,11 @@ public class Vector2D
 	}
 
 	/**
-	 * Creates the from point.
+	 * Creates a new {@code Vector2D} from a {@code Point2D} object. The {@code Vector2D} extends from the origin to the specified {@code Point2D}.
 	 *
 	 * @param p
-	 *            the p
-	 * @return the vector2 d
+	 *            the {@code Point2D}
+	 * @return the {@code Vector2D} that measures the displacement of the {@code Point2D} from the origin.
 	 */
 	public static Vector2D createFromPoint(final Point2D p)
 	{
@@ -50,13 +79,13 @@ public class Vector2D
 	}
 
 	/**
-	 * Creates the from polar.
+	 * Creates a new {@code Vector2D} from a polar point. The resulting {@code Vector2D} measures the displacement from the origin.
 	 *
 	 * @param theta
-	 *            the theta
+	 *            the angle between a line intersecting both the origin and this point and the positive x-axis
 	 * @param rho
-	 *            the rho
-	 * @return the vector2 d
+	 *            the distance from the origin to this point
+	 * @return a {@code Vector2D} that measures the displacement of this point from the origin.
 	 */
 	public static Vector2D createFromPolar(final double theta, final double rho)
 	{
@@ -64,11 +93,11 @@ public class Vector2D
 	}
 
 	/**
-	 * Creates the from polar point.
+	 * Creates a new {@code Vector2D} from a {@link PolarPoint}. The {@code Vector2D} measures the displacement of this {@code PolarPoint} from the origin.
 	 *
 	 * @param p
-	 *            the p
-	 * @return the vector2 d
+	 *            the {@code PolarPoint}
+	 * @return a {@code Vector2D} that measures the displacement from the {@code PolarPoint} to the origin.
 	 */
 	public static Vector2D createFromPolarPoint(final PolarPoint p)
 	{
@@ -199,5 +228,19 @@ public class Vector2D
 	public void setMagnitude(final double magnitude)
 	{
 		this.magnitude = magnitude;
+	}
+
+	public void paintAt(double x, double y, Graphics2D g2d)
+	{
+		g2d.setPaint(new GradientPaint((float) x, (float) y, Color.BLUE, (float) (x + this.getX()), (float) (y + this.getY()), Color.RED));
+		g2d.drawLine((int) x, (int) y, (int) (x + this.getX()), (int) (y + this.getY()));
+		Path2D.Double p2d = new Path2D.Double();
+		PolarPoint p1 = new PolarPoint(this.getMagnitude() / 5, this.getDirection() + (7 * Math.PI / 8));
+		PolarPoint p2 = new PolarPoint(this.getMagnitude() / 5, this.getDirection() - (7 * Math.PI / 8));
+		p2d.moveTo(x + this.getX(), y + this.getY());
+		p2d.lineTo(x + this.getX() + p1.getX(), y + this.getY() + p1.getY());
+		p2d.lineTo(x + this.getX() + p2.getX(), y + this.getY() + p2.getY());
+		p2d.closePath();
+		g2d.fill(p2d);
 	}
 }
