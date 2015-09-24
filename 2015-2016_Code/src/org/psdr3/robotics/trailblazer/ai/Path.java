7177be6342f7ccc99f052e5f464693952d4b03ec
Mitchell Skaggs <skaggsm333@gmail.com>
2015-09-24 17:39:02 -0500

    Added groundwork AI and data structures.

---
+++ 2015-2016_Code/src/org/psdr3/robotics/trailblazer/ai/Path.java
@@ -0,0 +1,30 @@
+package org.psdr3.robotics.trailblazer.ai;
+
+import org.psdr3.robotics.trailblazer.Tile;
+
+import java.awt.geom.Path2D;
+import java.util.LinkedList;
+
+/**
+ * Created by skaggsm on 9/17/15.
+ */
+public class Path {
+	LinkedList<Tile> tiles;
+
+	public Path() {
+		this.tiles = new LinkedList<Tile>();
+	}
+
+	public void append(Tile tile) {
+		this.tiles.add(tile);
+	}
+
+	public Path2D.Double getFancyPath() {
+		Path2D.Double fancyPath = new Path2D.Double();
+		fancyPath.moveTo(this.tiles.getFirst().getX() + 0.5, this.tiles.getFirst().getY() + 0.5);
+		for (Tile tile : this.tiles.subList(1, this.tiles.size())) {
+			fancyPath.lineTo(tile.getX() + 0.5, tile.getY() + 0.5);
+		}
+		return fancyPath;
+	}
+}
---
+++ 2015-2016_Code/src/org/psdr3/robotics/trailblazer/math/Line.java
@@ -0,0 +1,78 @@
+package org.psdr3.robotics.trailblazer.math;
+
+import org.apache.commons.math3.fraction.BigFraction;
+
+import java.util.function.Function;
+
+/**
+ * Created by skaggsm on 9/19/15.
+ */
+public class Line {
+	BigFraction a, b, c;
+
+	public Line(double a, double b, double c) {
+		this.a = new BigFraction(a);
+		this.b = new BigFraction(b);
+		this.c = new BigFraction(c);
+	}
+
+	public Line(BigFraction a, BigFraction b, BigFraction c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+
+	public Function<BigFraction, BigFraction> getXToYFunction() {
+		if (this.isVertical()) {
+			throw new UnsupportedOperationException("Not a function of X; vertical line.");
+		} else if (this.isHorizontal()) {
+			return new Function<BigFraction, BigFraction>() {
+				@Override
+				public BigFraction apply(BigFraction bigFraction) {
+					return c.divide(b);
+				}
+			};
+		} else {
+			return new Function<BigFraction, BigFraction>() {
+				@Override
+				public BigFraction apply(BigFraction bigFraction) {
+
+					return c.subtract(a.multiply(bigFraction)).divide(b);
+				}
+			};
+		}
+	}
+
+	public boolean isVertical() {
+		return this.b.equals(BigFraction.ZERO);
+	}
+
+	public boolean isHorizontal() {
+		return this.a.equals(BigFraction.ZERO);
+	}
+
+	public Function<BigFraction, BigFraction> getYToXFunction() {
+		if (this.isHorizontal()) {
+			throw new UnsupportedOperationException("Not a function of Y; horizontal line.");
+		} else if (this.isVertical()) {
+			return new Function<BigFraction, BigFraction>() {
+				@Override
+				public BigFraction apply(BigFraction bigFraction) {
+					return c.divide(a);
+				}
+			};
+		} else {
+			return new Function<BigFraction, BigFraction>() {
+				@Override
+				public BigFraction apply(BigFraction bigFraction) {
+
+					return c.subtract(b.multiply(bigFraction)).divide(a);
+				}
+			};
+		}
+	}
+
+	public boolean isOblique() {
+		return !this.isVertical() && !this.isHorizontal();
+	}
+}
